5b173d3c1e1e4f3e158901bb65c0a36c7c30d8bc
amos.wang <amos.wang>
2021-02-01 10:03:33 +0800

    feat: add des crypto

---
+++ common-util/src/main/java/com/amos/common/util/crypto/CryptoConstant.java
@@ -0,0 +1,20 @@
+package com.amos.common.util.crypto;
+
+/**
+ * DESCRIPTION: CryptoConstant
+ *
+ * @author <a href="mailto:amos.wang@xiaoi.com">amos.wang</a>
+ * @date 2/18/2020
+ */
+public interface CryptoConstant {
+
+    char CHAT_ONE = '0';
+    char CHAT_NINE = '9';
+    char CHAT_LOWER_A = 'a';
+    char CHAT_LOWER_F = 'f';
+    char CHAT_UPPER_A = 'A';
+    char CHAT_UPPER_F = 'F';
+
+    int MIN_KEN_LEN = 8;
+
+}
